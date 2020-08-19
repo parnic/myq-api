cc8f82b2ca3a0019d81bb7bbb89a02c582cb4493
Parnic <github@parnic.com>
2020-08-19 09:32:57 -0500

    Run from the local copy, not code staged for publishing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const MyQ = require('./dist/myq');
-const constants = require('./dist/constants');
+const MyQ = require('./src/myq');
+const constants = require('./src/constants');
 
 module.exports = {
   default: MyQ,
